00f1fb7d364904cea6ad0f594a01840258a2a103
Ben Leggiero <BenLeggiero@Gmail.com>
2018-11-29 00:31:39 -0500

    WIP: Moving stuff to BHSP libraries
    * Also working out ideal value functions
    * Also tweaking compilation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package DecisionCruncher
 
 
+import DecisionCruncher.Requirement.IdealValue.*
 import jQueryInterface.*
 
 
@@ -12,20 +13,22 @@ import jQueryInterface.*
 fun main(args: Array<String>) {
     jq {
         val requirements = listOf(
-                Requirement(id = "c", name = "Cost", idealValue = 1400.0, category = "Financial", importance = 1.0),
-                Requirement(id = "d", name = "Distance to QT", category = "QoL", importance = 0.6)
+                Requirement(id = "c", name = "Cost", category = "Financial", importance = 1.0,
+                            idealValue = fullRange(lowestAcceptableInput = 1000.0, idealValue = 1400.0, highestAcceptableInput = 1800.0, unit = "$")),
+                Requirement(id = "d", name = "Distance to QT", category = "QoL", importance = 0.6,
+                            idealValue = lowerIsBetter(highestAcceptableInput = 5.0, unit = "miles"))
         )
 
         val options: List<Option> = listOf(
                 Option("Century Skyline",
-                       mapOf("c" to 0.5,
-                             "d" to 0.9)),
+                       mapOf("c" to RequirementConformance(0.5),
+                             "d" to RequirementConformance(0.9))),
                 Option("Somewhere Else A",
-                       mapOf("c" to 0.7,
-                             "d" to 0.7)),
+                       mapOf("c" to RequirementConformance(0.7),
+                             "d" to RequirementConformance(0.7))),
                 Option("Somewhere Else B",
-                       mapOf("c" to 1.0,
-                             "d" to 0.4))
+                       mapOf("c" to RequirementConformance(1.0),
+                             "d" to RequirementConformance(0.4)))
         )
 
         console.log(DecisionCrunchEngine.crunch(allRequirements = requirements, allOptions = options).pieChart.toList().toTypedArray())
